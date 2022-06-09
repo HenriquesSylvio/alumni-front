65e49189221525dc8a213d7878a6444807084dc7
HenriquesSylvio <Henriques.Sylvio@outlook.fr>
2022-06-09 20:56:45 +0200

    📦 NEW: Add Layout components

---
+++ src/components/Layout/Header/Header.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+
+export default function Header() {
+    return (
+        <header>Header</header>
+    )
+}
