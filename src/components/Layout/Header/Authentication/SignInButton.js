b64badd69d735c04a4ea6ab53f71d36d09a836ec
HenriquesSylvio <Henriques.Sylvio@outlook.fr>
2022-06-10 22:39:55 +0200

    👌 IMPROVE: Add Modal when clicking to SignInButton Component

--- src/components/Layout/Header/Authentication/SignInButton.js
+++ src/components/Layout/Header/Authentication/SignInButton.js
@@ -1,13 +1,51 @@
 import * as React from 'react';
-import {Button} from "@mui/material";
+import {Button, Fade, Modal} from "@mui/material";
 import Box from "@mui/material/Box";
+import Backdrop from '@mui/material/Backdrop';
+import Typography from "@mui/material/Typography";
+
+
+const style = {
+    position: 'absolute',
+    top: '25%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 'auto',
+    bgcolor: 'background.paper',
+    boxShadow: 24,
+    p: 4,
+};
 
 export default function SignInButton() {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false)
+
     return (
         <Box sx={{ p: 1 }}>
-            <Button color="inherit" variant="outlined">
+            <Button onClick={handleOpen} color="inherit" variant="outlined">
                 Se connecter
             </Button>
+            <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                    timeout: 500,
+                }}
+            >
+
+                <Fade in={open}>
+                    <Box sx={style}>
+                        <Typography>
+                            Test
+                        </Typography>
+                    </Box>
+                </Fade>
+            </Modal>
         </Box>
     );
 }
