34dc523ce55e269078f1a967977504db91f54d4f
tcar <thomasulrac@gmail.com>
2022-06-10 12:33:09 +0200

    📦 NEW: First version of inscription

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import Button from '@mui/material/Button';
+import ModalDialog from './ModalDialog';
+import Inscription from "./pages/inscription";
+import { HashRouter, Route, Switch } from "react-router-dom";
 
-function App() {
-  return <div>Hello world!</div>;
-}
+const App = () => {
+  // declare a new state variable for modal open
+  const [open, setOpen] = useState(false);
+
+  // function to handle modal open
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  // function to handle modal close
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div className="App">
+      <Button variant="contained" color="primary" onClick={handleOpen}>
+        Signup
+      </Button>
+      <ModalDialog open={open} handleClose={handleClose} />
+    <HashRouter>
+      <Route exact path='/Inscription' component={Inscription}/> 
+    </HashRouter>
+    </div>
+   
+  );
+};
+
+
 
 export default App;
