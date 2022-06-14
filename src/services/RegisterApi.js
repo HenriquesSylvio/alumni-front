075d137ead86fee5687ccadc765d35acb8072344
tcar <thomasulrac@gmail.com>
2022-06-14 10:28:01 +0200

    👌 IMPROVE: Improved routing

---
+++ src/services/RegisterApi.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+import { addItem } from "./LocaleStorage";
+
+const URLApi = "http://127.0.0.1:8080/api/"
+
+export function register(credentials) {
+    return axios
+        .post(URLApi + "register", credentials);
+}
