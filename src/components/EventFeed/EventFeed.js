0d9d72bb7693cca78cce47bbc5eeba678b92e9db
tcar <thomasulrac@gmail.com>
2022-06-14 17:00:36 +0200

    🐛 FIX: modif description

--- src/components/EventFeed/EventFeed.js
+++ src/components/EventFeed/EventFeed.js
@@ -5,7 +5,7 @@ import { Button } from "@mui/material";
 
 export default function EventFeed({titre, description, couleur, ...rest}) {
     return (
-        <Box sx={{border: '1px solid grey', p: 2, borderRadius: 2, color: couleur, ...rest}}>
+        <Box sx={{border: '1px solid grey', p: 1, borderRadius: 2, color: couleur, ...rest}}>
             <h3>{titre}</h3>
             <p>{description}</p>
             <Button>En savoir plus</Button>
--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -6,12 +6,13 @@ import EventFeed from '../components/EventFeed/EventFeed';
 export default function Feed() {
     return (
         <div style={{display: "flex"}}>
-            <Box sx={{display:"flex", flex: 1, border: '1px solid grey'}}><h1>Feed Page</h1></Box>
-            <div style={{paddingRight:'50px'}}>
+            <Box sx={{display:"flex", flex: 1, border: '1px dashed grey'}}><h1>Feed Page</h1></Box>
+
+            <div style={{width:"25%"}}>
                 <Stack className="event-right" direction="column" justifyContent="space-evenly" alignItems="center" spacing={4} marginBottom={5}>
                     <h2>Prochains évènements</h2>
                     {/* Box personnalisée pour les feeds à droite*/}
-                    <EventFeed titre="Titre 1" description="texte 1" couleur="orange"/>
+                    <EventFeed titre="Titre 1" couleur="#CA4B38" description="texte 1 texte 1 texte 1  texte 1  texte 1  texte 1  texte 1  texte 1  texte 1  texte 1  texte 1  texte 1  texte 1  texte 1  texte 1  texte 1  texte 1  texte 1  texte 1 "/>
                     <EventFeed titre="Titre 3" description="texte 3"/>
                     <EventFeed titre="Titre 5" description="texte 5"/>
                 </Stack>
@@ -24,6 +25,5 @@ export default function Feed() {
                 </Stack>
             </div>
         </div>
-
     );
 }
