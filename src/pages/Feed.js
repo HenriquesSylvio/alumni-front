542360bbfc1431ec7fd5bfd129b90e4f1b09f52c
tcar <thomasulrac@gmail.com>
2022-06-15 11:38:28 +0200

    📦 NEW: MainFeed articles

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -2,14 +2,17 @@ import { Button, Stack } from '@mui/material';
 import { Box, padding } from '@mui/system';
 import * as React from 'react';
 import EventFeed from '../components/EventFeed/EventFeed';
+import MainFeed from '../components/MainFeed/MainFeed';
 
 export default function Feed() {
     return (
         <div style={{display: "flex"}}>
-            <Box sx={{display:"flex", flex: 1, border: '1px dashed grey'}}>
-                <h1>Feed Page</h1>
-                
-            </Box>
+            <Stack direction="column" alignItems="center" spacing={5} sx={{display:"flex", flex: 1, border: '1px dashed grey', color:"#CA4B38"}}>
+                <h1>Fil d'actualités</h1>
+                <MainFeed titre="La Normandie Web School recrute !" description="Nouvelle école dans le numérique, l'administration à besoin de vous, recherche tel type d'emploi"></MainFeed>
+                <MainFeed titre="La Normandie Web School recrute !" description="Nouvelle école dans le numérique, l'administration à besoin de vous, recherche tel type d'emploi"></MainFeed>
+                <MainFeed titre="La Normandie Web School recrute !" description="Nouvelle école dans le numérique, l'administration à besoin de vous, recherche tel type d'emploi"></MainFeed>
+            </Stack>
 
             <div style={{width:"25%"}}>
                 <Stack className="event-right" direction="column" justifyContent="space-evenly" alignItems="center" spacing={4} marginBottom={5} color="#CA4B38">
