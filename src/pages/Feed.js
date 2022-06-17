e14134c8e2893d02acd39a42cc4aa6ada1c8a2c4
HenriquesSylvio <henriques.sylvio@outlook.fr>
2022-06-17 11:05:11 +0200

    👌 IMPROVE: The page can display information from the json

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,49 +1,66 @@
-import { Button, Stack } from '@mui/material';
+import {Button, Card, Stack} from '@mui/material';
 import { Box, padding } from '@mui/system';
-import React, {useContext, useEffect, useState} from 'react';
+import React, {Component, useContext, useEffect, useState} from 'react';
 import EventFeed from '../components/EventFeed/EventFeed';
 import MainFeed from '../components/MainFeed/MainFeed';
 import getFeed from "../services/FeedApi";
+import SignInButton from "../components/Layout/Header/LoginRegister/SignInButton";
 
-export default function Feed() {
-    let posts = [];
-    // const [posts, setPosts] = useState([]);
-    useEffect(() => {    // Met à jour l    e titre du document via l’API du navigateur
-        getFeeds().then(r => r);
-    }, []);
+class Feed extends Component {
 
-    const getFeeds = async () => {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            posts: [],
+        }
+    }
+
+    async getFeed() {
         const response = await getFeed(1);
-        // setPosts(...posts, response.data.data)
-        posts = response.data.data;
-        console.log(posts.length)
+        this.setState({posts: response.data.data})
     };
 
-    return (
-        <div style={{display: "flex"}}>
-            <Stack direction="column" alignItems="center" spacing={5} sx={{display:"flex", flex: 1, color:"#CA4B38"}}>
-                <h1>Fil d'actualités</h1>
-                <MainFeed titre="La Normandie Web School recrute !" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ex augue, fringilla nec facilisis eu, gravida in felis. Ut vitae augue nec nunc dignissim accumsan. Phasellus consequat molestie convallis. Curabitur consequat neque eu risus consequat rhoncus. In vulputate vehicula finibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque tincidunt blandit nulla ac accumsan. Class aptent taciti sociosqu ad litora torquent per conubia molestie. "></MainFeed>
-                <MainFeed titre="La Normandie Web School recrute !" description="Nouvelle école dans le numérique, l'administration a besoin de vous, recherche tel type d'emploi"></MainFeed>
-                <MainFeed titre="La Normandie Web School recrute !" description=""></MainFeed>
-            </Stack>
+    async componentDidMount() {
+        await this.getFeed();
+    }
 
-            <div style={{width:"25%"}}>
-                <Stack className="event-right" direction="column" justifyContent="space-evenly" alignItems="center" spacing={4} marginBottom={5} color="#CA4B38">
-                    <h2>Prochains évènements</h2>
-                    {/* Box personnalisée pour les feeds à droite*/}
-                    <EventFeed titre="Titre 1" description="texte 1 texte 1 texte 1  texte 1  texte 1  texte 1  texte 1  texte 1  texte 1  texte 1  texte 1  texte 1  texte 1  texte 1  texte 1  texte 1  texte 1  texte 1  texte 1 "/>
-                    <EventFeed titre="Titre 3" description="texte 3"/>
-                    <EventFeed titre="Titre 5" description="texte 5"/>
-                </Stack>
+    render() {
+        const { posts } = this.state
+        return (
+            <div style={{display: "flex"}}>
+                <Stack direction="column" alignItems="center" spacing={5} sx={{display:"flex", flex: 1, color:"#CA4B38"}}>
+                    <h1>Fil d'actualités</h1>
+                    {
+                        posts.length ?
+                            posts.map(post =>
+                                // <MainFeed titre="La Normandie Web School recrute !" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ex augue, fringilla nec facilisis eu, gravida in felis. Ut vitae augue nec nunc dignissim accumsan. Phasellus consequat molestie convallis. Curabitur consequat neque eu risus consequat rhoncus. In vulputate vehicula finibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque tincidunt blandit nulla ac accumsan. Class aptent taciti sociosqu ad litora torquent per conubia molestie. "></MainFeed>
+                                // <MainFeed titre="La Normandie Web School recrute !" description="Nouvelle école dans le numérique, l'administration a besoin de vous, recherche tel type d'emploi"></MainFeed>
+                                <MainFeed titre="La Normandie Web School recrute !" description={post.content}></MainFeed>
+                            ): null
+                    }
+                            </Stack>
+                            <div style={{width:"25%"}}>
+                                <Stack className="event-right" direction="column" justifyContent="space-evenly" alignItems="center" spacing={4} marginBottom={5} color="#CA4B38">
+                                    <h2>Prochains évènements</h2>
+                                    {/* Box personnalisée pour les feeds à droite*/}
+                                    <EventFeed titre="Titre 1" description="texte 1 texte 1 texte 1  texte 1  texte 1  texte 1  texte 1  texte 1  texte 1  texte 1  texte 1  texte 1  texte 1  texte 1  texte 1  texte 1  texte 1  texte 1  texte 1 "/>
+                                    <EventFeed titre="Titre 3" description="texte 3"/>
+                                    <EventFeed titre="Titre 5" description="texte 5"/>
+                                </Stack>
 
-                <Stack className="event-right" direction="column" justifyContent="space-evenly" alignItems="center" spacing={5} color="#CA4B38">
-                    <h2>Offres d'emploi</h2>
-                    <EventFeed titre="Titre 1" description="texte 1"/>
-                    <EventFeed titre="Titre 3" description="texte 3"/>
-                    <EventFeed titre="Titre 5" description="texte 5"/>
-                </Stack>
+                                <Stack className="event-right" direction="column" justifyContent="space-evenly" alignItems="center" spacing={5} color="#CA4B38">
+                                    <h2>Offres d'emploi</h2>
+                                    <EventFeed titre="Titre 1" description="texte 1"/>
+                                    <EventFeed titre="Titre 3" description="texte 3"/>
+                                    <EventFeed titre="Titre 5" description="texte 5"/>
+                                </Stack>
+                            </div>
+                    }
             </div>
-        </div>
-    )
+        )
+    }
 }
+
+
+export default Feed
