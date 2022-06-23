8946c98458c5a176807e29d7db4ac0c0df61a2dd
HenriquesSylvio <Henriques.Sylvio@outlook.fr>
2022-06-23 16:23:28 +0200

    👌 IMPROVE: Add spinner when loading Profile page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, {useState} from 'react';
+import React, {useState, Suspense, lazy} from 'react';
 import Home from './pages/home';
 import Inscription from './pages/inscription';
 import Feed from './pages/Feed';
@@ -10,7 +10,10 @@ import Auth from "./contexts/Auth";
 import {hasAuthenticated} from "./services/AuthApi";
 import {ToastContainer} from "react-toastify";
 import AuthenticatedRoute from "./contexts/AuthenticatedRoute";
+import {CircularProgress} from "@mui/material";
+import Box from "@mui/material/Box";
 
+// const Profile = lazy(() => import('./pages/Profile'))
 const App = () => {
     const [isAuthenticated, setIsAuthenticated] = useState(hasAuthenticated());
     document.body.style = 'background: #F2F2F2;';
@@ -19,14 +22,24 @@ const App = () => {
       <Auth.Provider value={{isAuthenticated, setIsAuthenticated}}>
           <BrowserRouter>
               <Layout>
-                  <Routes>
-                      <Route path='/' element={<Home/>}/>
-                      <Route exact path='/inscription' element={<Inscription/>}/>
-                      <Route element={<AuthenticatedRoute/>}>
-                          <Route exact path='/feed' element={<Feed/>}/>
-                          <Route exact path='/profile' element={<Profile/>}/>
-                      </Route>
-                  </Routes>
+                  {/*<Suspense fallback={*/}
+                  {/*    <Box sx={{*/}
+                  {/*        display: 'flex',*/}
+                  {/*        flexDirection: 'column',*/}
+                  {/*        alignItems: 'center',*/}
+                  {/*        p:50*/}
+                  {/*    }}>*/}
+                  {/*        <CircularProgress sx={{justifyContent:"center", display:"flex"}}/></Box>*/}
+                  {/*}>*/}
+                      <Routes>
+                          <Route path='/' element={<Home/>}/>
+                          <Route exact path='/inscription' element={<Inscription/>}/>
+                          <Route element={<AuthenticatedRoute/>}>
+                              <Route exact path='/feed' element={<Feed/>}/>
+                              <Route exact path='/profile' element={<Profile/>}/>
+                          </Route>
+                      </Routes>
+                  {/*</Suspense>*/}
                   <ToastContainer />
               </Layout>
 
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import {Box} from "@mui/system";
-import {Stack} from "@mui/material";
+import {CircularProgress, Stack} from "@mui/material";
 import Typography from "@mui/material/Typography";
 import MainFeed from "../components/MainFeed/MainFeed";
 import DetailUser from "../components/Profile/DetailUser";
@@ -25,6 +25,7 @@ class Profile extends Component {
 
         this.state = {
             user: [],
+            loadingPage: true
         }
     }
 
@@ -36,10 +37,11 @@ class Profile extends Component {
 
     async componentDidMount() {
         await this.getProfileUser();
+        this.setState({loadingPage: false})
     }
 
     render() {
-        const { user } = this.state
+        const { user, loadingPage} = this.state
         return (
             <Box
                 sx={{
@@ -48,31 +50,44 @@ class Profile extends Component {
                     alignItems: 'center',
                 }}
             >
-                <Box sx={{m: 1, background: 'rgba(202,75,56,0.25)'}} paddingTop={10} minHeight={225}>
-                    <Typography paddingRight={20} paddingLeft={20} paddingBottom={5} variant="h6" component="div">
-                        {user.last_name} {user.first_name}
-                    </Typography>
-                    <Typography paddingRight={20} paddingLeft={20} variant="subtitle1" component="div" color={"grey"}>
-                        {user.biography}
-                    </Typography>
+                {loadingPage ? (
+                    <Box sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        marginTop: 2
+                    }}>
+                        <CircularProgress sx={{justifyContent:"center", display:"flex"}}/>
+                    </Box>
+                ) : (
+                    <>
+                        <Box sx={{m: 1, background: 'rgba(202,75,56,0.25)'}} paddingTop={10} minHeight={225}>
+                            <Typography paddingRight={20} paddingLeft={20} paddingBottom={5} variant="h6" component="div">
+                                {user.last_name} {user.first_name}
+                            </Typography>
+                            <Typography paddingRight={20} paddingLeft={20} variant="subtitle1" component="div" color={"grey"}>
+                                {user.biography}
+                            </Typography>
 
-                </Box>
-                <Box style={{display: 'flex'}}>
-                    <Box width="95%" marginLeft={5} marginRight={5} marginTop={-9}>
+                        </Box>
+                        <Box style={{display: 'flex'}}>
+                        <Box width="95%" marginLeft={5} marginRight={5} marginTop={-9}>
                         <Typography marginLeft={5} variant="h6" component="div">
-                            Postes récents
+                        Postes récents
                         </Typography>
                         <Stack direction='column' spacing={5}>
-                            <MainFeed
-                                titre='La Normandie Web School recrute !'
-                                description="Nouvelle école dans le numérique, l'administration à besoin de vous, recherche tel type d'emploi"
-                                nbComment={5}></MainFeed>
+                        <MainFeed
+                        titre='La Normandie Web School recrute !'
+                        description="Nouvelle école dans le numérique, l'administration à besoin de vous, recherche tel type d'emploi"
+                        nbComment={5}></MainFeed>
                         </Stack>
-                    </Box>
-                    <Box marginLeft={5} marginRight={5} marginTop={-5}>
+                        </Box>
+                        <Box marginLeft={5} marginRight={5} marginTop={-5}>
                         <DetailUser first_name={user.first_name} last_name={user.last_name} urlProfilePicture={user.url_profile_picture} nbSubscriber={user.followerNumber} nbPosts='5' nbSubscription={user.followingNumber} promo={user.promo} sector='Développeur'/>
-                    </Box>
-                </Box>
+                        </Box>
+                        </Box>
+                    </>
+                )}
             </Box>
         );
     }
