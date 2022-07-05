29c79db023b25701a1866f3c22933f4b16a9080b
HenriquesSylvio <Henriques.Sylvio@outlook.fr>
2022-07-05 22:53:40 +0200

    👌 IMPROVE: Add validator for AddPostForm components

--- src/components/MainFeed/AddPostForm.js
+++ src/components/MainFeed/AddPostForm.js
@@ -17,17 +17,21 @@ import CreateIcon from '@mui/icons-material/Create';
 import {addPost} from "../../services/AddPostApi";
 import getTag from "../../services/GetTagApi";
 import {Autocomplete} from "@mui/lab";
+import validate from "../../validators/AddPostValidator";
 
 const theme = createTheme();
 
 export default function AddPostForm() {
+    const [errors, setErrors] = useState({});
     const [values, setValues] = useState({
+        title: "",
         content: "",
         tag: {
             id: ""
         }
     });
     const [tags, setTags] = useState([]);
+
     const listTag = [];
 
     const loadTag = async () => {
@@ -39,6 +43,10 @@ export default function AddPostForm() {
     useLayoutEffect(() => {
         loadTag()
     }, []);
+
+    function handleClick() {
+        setErrors(validate(values));
+    }
 
     // useEffect(async () => {
     //     await loadTag();
@@ -54,8 +62,9 @@ export default function AddPostForm() {
     const handleSubmit = async event => {
         event.preventDefault();
 
-        // await setErrors(validate(values));
-        // if (Object.keys(errors).length === 0) {
+        await setErrors(validate(values));
+        console.log(errors);
+        if (Object.keys(errors).length === 0) {
         //     setLoading(true);
         //     try {
         const response = await addPost(values);
@@ -68,7 +77,7 @@ export default function AddPostForm() {
         //         setLoading(false);
         //         console.log(response)
         //     }
-        // }
+        }
     };
 
 
@@ -100,6 +109,9 @@ export default function AddPostForm() {
                                     id="title"
                                     label="Titre"
                                     autoFocus
+                                    onChange={handleChange}
+                                    error={ errors.title }
+                                    helperText={ errors.title }
                                 />
                             </Grid>
                             <Grid item xs={12}>
@@ -112,6 +124,8 @@ export default function AddPostForm() {
                                     multiline
                                     rows={10}
                                     onChange={handleChange}
+                                    error={ errors.content }
+                                    helperText={ errors.content }
                                 />
                             </Grid>
                             <Grid item xs={12}>
@@ -119,15 +133,15 @@ export default function AddPostForm() {
                                     fullWidth
                                     options={tags}
                                     onChange={(event, value) => setValues({...values, ["tag"]: value})}
-
                                     autoComplete="tag"
                                     renderInput={(params) =>
                                         <TextField {...params}
                                                    required
-                                            // onChange={handleChange}
                                                    id="tag"
                                                    name="tag"
-                                                   label="tag"
+                                                   label="Tag"
+                                                   error={ errors.tag }
+                                                   helperText={ errors.tag }
                                         />
                                     }
                                 />
@@ -138,7 +152,7 @@ export default function AddPostForm() {
                             fullWidth
                             variant="contained"
                             sx={{ mt: 3, mb: 2 }}
-                            // onClick={handleClick}
+                            onClick={handleClick}
                             // loading={loading}
                             // loadingPosition="end"
                         >
@@ -149,119 +163,3 @@ export default function AddPostForm() {
             </Container>
     );
 }
-
-
-// import React, {useContext, useEffect, useState} from 'react';
-// import Button from '@mui/material/Button';
-// import CssBaseline from '@mui/material/CssBaseline';
-// import Box from '@mui/material/Box';
-// import Container from '@mui/material/Container';
-// import { createTheme, ThemeProvider } from '@mui/material/styles';
-// import TextField from "@mui/material/TextField";
-// import {toast} from "react-toastify";
-// import 'react-toastify/dist/ReactToastify.css';
-// import {CircularProgress} from "@mui/material";
-// import Auth from "../../contexts/Auth";
-// import Grid from "@mui/material/Grid";
-// import Avatar from "@mui/material/Avatar";
-// import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-// import Typography from "@mui/material/Typography";
-// import CreateIcon from '@mui/icons-material/Create';
-// import validate from "../../validators/LoginValidator";
-// import {login} from "../../services/AuthApi";
-// import {addPost} from "../../services/addPostApi";
-// import {useNavigate} from "react-router-dom";
-//
-// const theme = createTheme();
-//
-// export default function AddPostForm() {
-//
-//     const [values, setValues] = useState({
-//         content: ""
-//     });
-//
-//     const handleChange = ({currentTarget}) => {
-//         const {name, value} = currentTarget;
-//
-//         setValues({...values, [name]: value})
-//     }
-//
-//     const handleSubmit = async event => {
-//         event.preventDefault();
-//
-//         // await setErrors(validate(values));
-//         // if (Object.keys(errors).length === 0) {
-//         //     setLoading(true);
-//         //     try {
-//         const response = await addPost(values);
-//         console.log(response);
-//         //         setIsAuthenticated(response);
-//         //         navigate('/feed')
-//         //         toast.success('Bienvenue ! 😄')
-//         //     } catch ({response}) {
-//         //         toast.error(response.data.erreur + ' 😃')
-//         //         setLoading(false);
-//         //         console.log(response)
-//         //     }
-//         // }
-//     };
-//
-//     return (
-//         <Container component="main">
-//             <Grid
-//                 container
-//                 spacing={0}
-//                 direction="column"
-//                 alignItems="center"
-//                 justifyContent="center"
-//                 style={{ minHeight: '100%' }}
-//                 onSubmit={handleSubmit}
-//             >
-//                 <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
-//                     <CreateIcon />
-//                 </Avatar>
-//                 <Typography component="h1" variant="h5">
-//                     Création de poste
-//                 </Typography>
-//                 <Box component="form" noValidate sx={{ mt: 1 }}>
-//                     <Grid container spacing={2}>
-//                         <Grid item xs={12}>
-//                             <TextField
-//                                 name="title"
-//                                 required
-//                                 fullWidth
-//                                 id="title"
-//                                 label="Titre"
-//                                 autoFocus
-//                             />
-//                         </Grid>
-//                         <Grid item xs={12}>
-//                             <TextField
-//                                 required
-//                                 fullWidth
-//                                 id="content"
-//                                 name="content"
-//                                 label="Contenu du poste"
-//                                 multiline
-//                                 rows={10}
-//                                 onChange={handleChange}
-//                             />
-//                         </Grid>
-//                     </Grid>
-//                     <Button
-//                         type="submit"
-//                         fullWidth
-//                         variant="contained"
-//                         sx={{ mt: 3, mb: 2 }}
-//                         // onClick={handleClick}
-//                         // loading={loading}
-//                         // loadingPosition="end"
-//                     >
-//                         Créer
-//                     </Button>
-//                 </Box>
-//             </Grid>
-//         </Container>
-//         // </ThemeProvider>
-//     );
-// }
