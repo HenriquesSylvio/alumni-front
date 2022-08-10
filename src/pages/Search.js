31649129cdd468a31374a7881d1916bddae4dc73
HenriquesSylvio <henriques.sylvio@outlook.fr>
2022-08-10 19:28:28 +0200

    📦 NEW: Add Seach for post

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -10,17 +10,24 @@ import Backdrop from "@mui/material/Backdrop";
 import AddCommentForm from "../components/Post/AddCommentForm";
 // import {useContext} from "@types/react";
 import ActiveConnectedUser from "../contexts/ActiveConnectedUser";
+import getPosts from "../services/GetPostsApi";
 
 export default function Search() {
 
     const params = useParams();
     const [typeSearch, setTypeSearch] = useState("");
     const {activeProfile} = useContext(ActiveConnectedUser);
+    const [posts, setPost] = useState('');
 
     useEffect( () => {
+        const searchPosts = async () => {
+            const response = await getPosts(1, "test");
+            setPost(response.data.data)
+        }
+
         setTypeSearch(params.typeSearch)
         if (params.typeSearch === "user" ) {
-
+            searchPosts()
         }
         console.log(typeSearch)
     }, [params.typeSearch]);
@@ -45,31 +52,40 @@ export default function Search() {
                         myProfile={true}
                     />
                 </Grid>
-            {(typeSearch === "user" && (
-
-                        <Grid item xs>
-                            <DetailUser
-                                firstName={activeProfile.firstName}
-                                lastName={activeProfile.lastName}
-                                urlProfilePicture={activeProfile.urlProfilePicture}
-                                nbSubscriber={activeProfile.followerNumber}
-                                nbPosts='5'
-                                nbSubscription={activeProfile.followingNumber}
-                                promo={activeProfile.promo}
-                                sector='Développeur'
-                                biography={activeProfile.biography}
-                                idUser={activeProfile.id}
-                                subscribe={activeProfile.subcribe}
-                                canModify={false}
-                                myProfile={true}
-                            />
-                        </Grid>
-
-                ))
-                ||
-                null
-            }
+                <Grid item xs>
+                    {(typeSearch === "user" && (
+                        posts.length ?
+                            posts.map(post =>
+                                <Box sx={{
+                                    marginBottom: 2
+                                }}>
+                                    <MainFeed
+                                        post={post}
+                                    />
+                                </Box>
+                            ): null
+                    )) || null}
+                </Grid>
             </Grid>
         </Box>
     );
 }
+
+
+// <Grid item xs>
+//     <DetailUser
+//         firstName={activeProfile.firstName}
+//         lastName={activeProfile.lastName}
+//         urlProfilePicture={activeProfile.urlProfilePicture}
+//         nbSubscriber={activeProfile.followerNumber}
+//         nbPosts='5'
+//         nbSubscription={activeProfile.followingNumber}
+//         promo={activeProfile.promo}
+//         sector='Développeur'
+//         biography={activeProfile.biography}
+//         idUser={activeProfile.id}
+//         subscribe={activeProfile.subcribe}
+//         canModify={false}
+//         myProfile={true}
+//     />
+// </Grid>
