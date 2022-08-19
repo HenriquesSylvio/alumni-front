e84fdbdaa04a94577e570d09af1e6c821d318b65
HenriquesSylvio <henriques.sylvio@outlook.fr>
2022-08-19 22:26:27 +0200

    📦 NEW: Add GetFacultyApi service

---
+++ src/services/GetFacultyApi.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+import { addItem } from "./LocaleStorage";
+
+const URLApi = "http://127.0.0.1:8080/api/"
+// const URLApi = "https://alumni-nws-api.herokuapp.com/api/"
+
+export function getFaculty(credentials) {
+    return axios
+        .post(`${URLApi}faculty`, credentials);
+}
